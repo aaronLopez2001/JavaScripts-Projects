1f52bf218040777264b6132aa64700bd7a73230b
aaronLopez2001 <105614197+aaronLopez2001@users.noreply.github.com>
2022-06-11 16:07:07 -0700

    Add files via upload

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,31 @@
+
+document.write ("Hello World!") //Writes "Hello World" into the webpage
+var B="Warning! Your computer may have viruses!"; //creating a variable, naming it "B" and setting it's values.
+window.alert(B); //Creates a pop up that writes whatever the value of "B".
+
+
+
+
+
+document.write("Sally\'s friend Mary had a little lamb"); //Writes a string data to the webpage
+var ConcatenatedString= "This is me" //Concatenating a string data
++ " trying to concatenate a String"// Concatenating a string data
++ "-Jendrix Lopez"; // Concatenating a string data
+document.write(ConcatenatedString); //Writing the "ConcatenatedString" variable to the webpage
+
+var Cities= Philippines= "Manila", Canada="Toronto", Japan="Tokyo", USA="New York", China="Beijing"; //Creating a variable and assigning multiple values to it.
+var Cities= Cities.fontcolor("#308C8F"); //setting a font color of #308C8F to the values of "Cities"
+document.write(Japan); //Writing the value of variable named "Japan" into the webpage
+
+
+var Orange ="This paragraph is colored Orange"; //Creating a variable named orange and setting a string value to it
+var Orange = Orange.fontcolor("#FFA500"); //Setting  a font color to the variable named "Orange"
+document.write (Orange);//Writing the string value of the variable "Orange" to the webpage
+
+var A="This is a string"; //Assigning a variable and naming it A with a value of a string data
+var A= A.fontcolor("#1387A1");// setting a font color to the variable "A"
+document.write(A);//Writing the Values of variable "A" to the webpage
+
+
+var expression="5*5"; //this is me learning expressions, I named the Variable "Expressions" and assign a value to it
+document.write (expression); //Writing the values of Variable "expressions" to the webpage
