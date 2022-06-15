1148721a687c05c7f78c73656addc77b5b0a05d4
aaronLopez2001 <105614197+aaronLopez2001@users.noreply.github.com>
2022-06-15 12:08:36 -0700

    Add files via upload

---
+++ Project4_dictionaries/Js/Main.js
@@ -0,0 +1,20 @@
+function my_Dictionary(){
+    var pencil  = {
+        Type : "Graphite",
+        Color : "Black",
+        functionality : "Write , erase", 
+    } ;
+    document.getElementById("Dictionary").innerHTML = pencil.Type;
+
+}
+
+function mine_Dictionary(){
+    var pencil  = {
+        Type : "Graphite",
+        Color : "Black",
+        functionality : "Write , erase", 
+    } ;
+    document.getElementById("Dictionaries").innerHTML = pencil.Type;
+
+}
+
